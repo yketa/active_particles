105027a8424bb6cb91ed3db57e7253d213b9fb30
Yann-Edwin Keta <yketa@users.noreply.github.com>
2018-03-07 17:10:15 -0800

    new MSD routine
    msd.py now enables to tune the initial time for the calculation of the MSD as well as the maximum number of snapshots to be loaded for calculations and period of times to explore

--- launch/msd_DPD_P_A.sh
+++ launch/msd_DPD_P_A.sh
@@ -4,16 +4,25 @@ if [[ "$DATA" == "" ]]; then
 fi
 
 export DATA_DIRECTORY=/home/yketa/hoomd/colmig_DPD_P_A/data/${DATA}
+export PARAMETERS_FILE=${PARAMETERS_FILE-${DATA_DIRECTORY}/param.pickle}
+export POSITION_FILE=${POSITION_FILE-${DATA_DIRECTORY}/position.csv}
+
+export INITIAL_FRAME=${INITIAL_FRAME-0}
+export SNAP_MAXIMUM=${SNAP_MAXIMUM-10}
+export SNAP_PERIOD=${SNAP_PERIOD-1}
+
+. /home/yketa/exponents.sh # exporting letters expressions and float conversion functions
+MSD_PAR=msd_I$(float_to_letters $INITIAL_FRAME)_M$(float_to_letters $SNAP_MAXIMUM)_P$(float_to_letters $SNAP_PERIOD)
 
 mkdir -p ${DATA_DIRECTORY}/out
-output_file=${DATA_DIRECTORY}/out/msd.out
+output_file=${DATA_DIRECTORY}/out/${MSD_PAR}.out
 > $output_file
 
-sbatch --job-name=msd_${DATA} ${CHAIN:+-d afterok:$CHAIN} <<EOF
+sbatch --job-name=${MSD_PAR}_${DATA} ${CHAIN:+-d afterok:$CHAIN} <<EOF
 #!/bin/bash
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:k80:1
-#SBATCH --output /home/yketa/hoomd/colmig_DPD_P_A/sub/out/msd_${DATA}.%j.out
+#SBATCH --output /home/yketa/hoomd/colmig_DPD_P_A/sub/out/${MSD_PAR}_${DATA}.%j.out
 #SBATCH --ntasks-per-node 1
 
 /home/yketa/miniconda3/bin/python3.6 /home/yketa/hoomd/scripts/msd.py >> $output_file
