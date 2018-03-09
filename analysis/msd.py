a2bf4cbac13cb1afc0336711468f014371ff4efd
Yann-Edwin Keta <yketa@users.noreply.github.com>
2018-03-08 23:26:50 -0800

    measurement includes standard deviation

--- analysis/msd.py
+++ analysis/msd.py
@@ -24,15 +24,15 @@ def mean_square_displacement(data_dir='/home/yketa/hoomd', param_file='param.pic
 	pos = lambda time: np.reshape(np.genfromtxt(fname=pos_file, delimiter=',', skip_header=init_frame + time*snap_period, max_rows=1)[:-1], (N, 2)) # array of positions at time time
 	times = lambda N, n: np.linspace(0, N - n - 1, min(snap_max, N - n), dtype=int) # list of time snapshots at which to evaluate the mean square displacement at n*dumps for N time snapshots
 
-	out_file = data_dir + '/msd_D' + float_to_letters(density) + '_V' + float_to_letters(vzero) + '_R' + float_to_letters(dr) + '_N' + float_to_letters(N) + '_I' + float_to_letters(init_frame) + '_M' + float_to_letters(snap_max) + '_P' + float_to_letters(snap_period) + '.csv' # MSD file
+	out_file = data_dir + '/msd_std_D' + float_to_letters(density) + '_V' + float_to_letters(vzero) + '_R' + float_to_letters(dr) + '_N' + float_to_letters(N) + '_I' + float_to_letters(init_frame) + '_M' + float_to_letters(snap_max) + '_P' + float_to_letters(snap_period) + '.csv' # MSD file
 	msd_dump = open(out_file, 'w') # MSD dump
 
 	for dt in range(1, Ntimes): # loop over all different increment of time
 		# CALCULATION
 		time = time_step*period_dump*dt*snap_period # time at which is evaluated the mean square displacement
-		msdis = np.mean([np.sum(((lambda positions: positions - np.mean(positions, axis=0))(pos(time + dt)) - (lambda positions: positions - np.mean(positions, axis=0))(pos(time)))**2, axis=1) for time in times(Ntimes, dt)])
+		msdis, stdev = (lambda val: (np.mean(val), np.std(val)))([np.sum(((lambda positions: positions - np.mean(positions, axis=0))(pos(time + dt)) - (lambda positions: positions - np.mean(positions, axis=0))(pos(time)))**2, axis=1) for time in times(Ntimes, dt)])
 		# OUTPUT
-		msd_dump.write(str("%e,%e\n" % (time, msdis)))
+		msd_dump.write(str("%e,%e,%e\n" % (time, msdis, stdev)))
 
 	msd_dump.close()
 
