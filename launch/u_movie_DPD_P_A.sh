94568167fd12cdc2bc6d973e6c4f0751af78058d
Yann-Edwin Keta <yketa@users.noreply.github.com>
2018-03-30 21:03:08 -0700

    proper job name

--- launch/u_movie_DPD_P_A.sh
+++ launch/u_movie_DPD_P_A.sh
@@ -11,15 +11,18 @@ export INITIAL_FRAME=${INITIAL_FRAME-0}
 export FRAME_PERIOD=${FRAME_PERIOD-1}
 export FRAME_MAXIMUM=${FRAME_MAXIMUM-1000}
 
+. /home/yketa/exponents.sh # exporting letters expressions and float conversion functions
+MOV_PAR=u_mov_I$(float_to_letters $INITIAL_FRAME)_M$(float_to_letters $FRAME_MAXIMUM)_P$(float_to_letters $FRAME_PERIOD)
+
 mkdir -p /home/yketa/hoomd/colmig_DPD_P_A/data/${DATA}/out
-output_file=/home/yketa/hoomd/colmig_DPD_P_A/data/${DATA}/out/u_mov_${DATA}.out
+output_file=/home/yketa/hoomd/colmig_DPD_P_A/data/${DATA}/out/${MOV_PAR}.out
 > $output_file
 
-sbatch --job-name=u_mov_${DATA} <<EOF
+sbatch --job-name=${MOV_PAR}_${DATA} <<EOF
 #!/bin/bash
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:k80:1
-#SBATCH --output /home/yketa/hoomd/colmig_DPD_P_A/sub/out/u_mov_${DATA}.%j.out
+#SBATCH --output /home/yketa/hoomd/colmig_DPD_P_A/sub/out/${MOV_PAR}_${DATA}.%j.out
 #SBATCH --ntasks-per-node 1
 
 /bin/bash /home/yketa/bin/_colmig_DPD_P_A_u_makemovie >> $output_file
