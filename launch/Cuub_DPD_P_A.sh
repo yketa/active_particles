f0d7ed53ba852d10b1331e784086c54c6f69dfa8
Yann-Edwin Keta <yketa@users.noreply.github.com>
2018-04-05 17:08:33 -0700

    several times in one launch

--- launch/Cuub_DPD_P_A.sh
+++ launch/Cuub_DPD_P_A.sh
@@ -5,7 +5,8 @@ fi
 
 DATA_DIRECTORY="/home/yketa/hoomd/colmig_DPD_P_A/data/${DATA}"
 export DATA_DIRECTORY=$DATA_DIRECTORY
-export TIME=${TIME-1}
+
+TIME=${TIME-1}
 export INITIAL_FRAME=${INITIAL_FRAME-`/home/yketa/miniconda3/bin/python3.6 <<EOF
 print(int(($(/home/yketa/bin/_colmig_DPD_P_A_data $DATA N_steps)//$(/home/yketa/bin/_colmig_DPD_P_A_data $DATA period_dump))/2))
 EOF
@@ -20,12 +21,17 @@ EOF
 
 export SHOW='False'
 
+TIME_LIST=( $TIME )
 . /home/yketa/exponents.sh # exporting letters expressions and float conversion functions
-CUU_TIME=Cuub_I$(float_to_letters $INITIAL_FRAME)_T$(float_to_letters $TIME)_M$(float_to_letters $INTERVAL_MAXIMUM)_C$(float_to_letters $N_CASES)
+if (( ${#TIME_LIST[@]} == 1 )); then
+	CUU_TIME=Cuub_I$(float_to_letters $INITIAL_FRAME)_T$(float_to_letters $TIME)_M$(float_to_letters $INTERVAL_MAXIMUM)_C$(float_to_letters $N_CASES)
+else
+	CUU_TIME=Cuub_I$(float_to_letters $INITIAL_FRAME)_M$(float_to_letters $INTERVAL_MAXIMUM)_C$(float_to_letters $N_CASES)
+fi
 
 mkdir -p ${DATA_DIRECTORY}/out
 output_file=${DATA_DIRECTORY}/out/${CUU_TIME}.out
-> $output_file
+echo "TIME: $TIME" > $output_file
 
 sbatch --job-name=${CUU_TIME}_${DATA} ${CHAIN:+-d afterok:$CHAIN} <<EOF
 #!/bin/bash
@@ -34,6 +40,9 @@ sbatch --job-name=${CUU_TIME}_${DATA} ${CHAIN:+-d afterok:$CHAIN} <<EOF
 #SBATCH --output /home/yketa/hoomd/colmig_DPD_P_A/sub/out/${CUU_TIME}_${DATA}.%j.out
 #SBATCH --ntasks-per-node 1
 
-/home/yketa/bin/_colmig_DPD_P_A_Cuub >> $output_file
+for TIME in $TIME; do
+	export TIME=\$TIME
+	/home/yketa/bin/_colmig_DPD_P_A_Cuub >> $output_file
+done
 EOF
 
