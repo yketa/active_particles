e58c1b8e7aad22e028be5fe38fae178a7e57e405
Yann-Edwin Keta <yketa@users.noreply.github.com>
2018-04-06 20:23:36 -0700

    possibility to take logarithmic time scale + final frame

--- launch/u_movie_DPD_P_A.sh
+++ launch/u_movie_DPD_P_A.sh
@@ -8,11 +8,16 @@ fi
 export DATA=$DATA
 
 export INITIAL_FRAME=${INITIAL_FRAME-0}
+export FINAL_FRAME=${FINAL_FRAME-10000}
 export FRAME_PERIOD=${FRAME_PERIOD-1}
 export FRAME_MAXIMUM=${FRAME_MAXIMUM-1000}
+export TIME_SCALE=${TIME_SCALE-LOG}
 
 . /home/yketa/exponents.sh # exporting letters expressions and float conversion functions
 MOV_PAR=u_mov_I$(float_to_letters $INITIAL_FRAME)_M$(float_to_letters $FRAME_MAXIMUM)_P$(float_to_letters $FRAME_PERIOD)
+
+MOV=u_${DATA}_I$(float_to_letters $INITIAL_FRAME)_M$(float_to_letters $FRAME_MAXIMUM)_P$(float_to_letters $FRAME_PERIOD)
+export MOVIE_DIRECTORY=${MOVIE_DIRECTORY-/home/yketa/hoomd/colmig_DPD_P_A/movie/${MOV}}
 
 mkdir -p /home/yketa/hoomd/colmig_DPD_P_A/data/${DATA}/out
 output_file=/home/yketa/hoomd/colmig_DPD_P_A/data/${DATA}/out/${MOV_PAR}.out
