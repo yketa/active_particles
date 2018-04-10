5e98e117007dd7b733eed742984363859c746873
Yann-Edwin Keta <yketa@users.noreply.github.com>
2018-04-09 18:34:51 -0700

    calculate histogram of local density

--- analysis/varN.py
+++ analysis/varN.py
@@ -1,31 +1,82 @@
 #! /home/yketa/miniconda3/bin/python3.6
 
-import numpy as np
-import pickle
 import os
 
-data_dir = os.environ['DATA_DIRECTORY'] if 'DATA_DIRECTORY' in os.environ else '/home/yketa/hoomd' # data directory
+import matplotlib as mpl
+if not('SHOW' in os.environ and eval(os.environ['SHOW'])):
+	mpl.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import matplotlib.cm as cmx
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-param_file = data_dir + '/' + os.environ['PARAMETERS_FILE'] if 'PARAMETERS_FILE' in os.environ else data_dir + '/param.pickle' # parameters file
-vel_file = data_dir + '/' + os.environ['VELOCITY_FILE'] if 'VELOCITY_FILE' in os.environ else data_dir + '/velocity.csv' # velocity data file
-varN_file = data_dir + '/' + os.environ['OUTPUT_FILE'] if 'OUTPUT_FILE' in os.environ else data_dir + '/varN.pickle' # number variance output file
+cmap = plt.cm.jet
 
-file = open(param_file, 'rb')
-N, a, pdi, N_sizes, density, box_size, kT, mu, k, vzero, dr, damp_bro, shear_rate, time_step, N_steps, period_dump, prep_steps = pickle.load(file)
-file.close()
+import numpy as np
 
-time = int(eval(os.environ['TIME'])) if 'TIME' in os.environ else -1 # time to evaluate the spatial variance of the number of particles
-Ncases = int(eval(os.environ['N_CASES'])) if 'N_CASES' in os.environ else int(np.sqrt(N)) + (1 - int(np.sqrt(N))%2) # number of cases in each direction to compute the number of neighbouring particles
-eval_points = int(eval(os.environ['EVALUATION_POINTS'])) if 'EVALUATION_POINTS' in os.environ else 100 # number of points of evaluation
+import gsd
+import gsd.hoomd
+import gsd.pygsd
 
-data = np.genfromtxt(fname=vel_file, delimiter=',', skip_header=time if time >= 0 else N_steps//period_dump - 1, max_rows=1) # positions and velocity at time time (time = -1 if time < 0 in input)
-positions = np.array([data[particle*3:particle*3 + 2] for particle in range(N)]) # array of the positions at time time
+import pickle
 
-number = lambda point, size: np.sum((abs((positions - np.array(point) + box_size/2)%box_size - box_size/2) <= size/2).all(axis=1)) # returns the number of particles in a square of size size centred on the point point
-points = lambda nb: np.array([(x, y) for y in np.linspace(- box_size/2, box_size/2, nb, endpoint=False) for x in np.linspace(- box_size/2, box_size/2, nb, endpoint=False)]) # returns nb x nb points linearly placed in the box
-varNumber = lambda size, nb: (lambda list: np.mean(list**2) - np.mean(list))(np.array(list(map(lambda point: number(point, size), points(nb))))) # returns the number variance for points in squares of size size centred around points linearly spaced in the box
+import sys
 
-out_file = open(varN_file, 'wb')
-size, varN = (lambda array: (array, list(map(lambda size: varNumber(size, Ncases), array))))(np.linspace(2*a, box_size, eval_points))
-pickle.dump([size, varN], out_file)
-out_file.close()
+sys.path.append('/home/yketa')
+from exponents import *
+
+import math
+from collections import OrderedDict
+
+data_dir = os.environ['DATA_DIRECTORY'] if 'DATA_DIRECTORY' in os.environ else os.getcwd() # data directory
+
+parameters_file = os.environ['PARAMETERS_FILE'] if 'PARAMETERS_FILE' in os.environ else data_dir + '/param.pickle' # parameters pickle file
+wrap_file_name = os.environ['WRAPPED_FILE'] if 'WRAPPED_FILE' in os.environ else data_dir + '/trajectory.gsd' # trajectory gsd file
+
+init_frame = int(eval(os.environ['INITIAL_FRAME'])) if 'INITIAL_FRAME' in os.environ else -1 # initial time for the calculation of the displacement correlation
+int_max = int(eval(os.environ['INTERVAL_MAXIMUM'])) if 'INTERVAL_MAXIMUM' in os.environ else 1 # maximum number of intervals taken for the calculation of the displacement correlation
+
+Ncases = int(eval(os.environ['N_CASES'])) if 'N_CASES' in os.environ else -1 # number of cases in each direction to compute the number of neighbouring particles
+
+max_box_size = float(eval(os.environ['MAX_BOX_SIZE'])) if 'MAX_BOX_SIZE' in os.environ else 10 # size of the square box in which particles are counted
+
+Nbins = int(eval(os.environ['N_BINS'])) if 'N_BINS' in os.environ else 10 # number of bins for the histogram
+
+with open(parameters_file, 'rb') as param_file:
+	N, a, pdi, N_sizes, density, box_size, kT, mu, k, vzero, dr, damp_bro, shear_rate, time_step, N_steps, period_dump, prep_steps = pickle.load(param_file)
+Nentries = N_steps//period_dump # number of frames
+prep_frames = prep_steps//period_dump # number of preparation frames
+
+init_frame = int(Nentries/2) if init_frame < 0 else init_frame
+frames = np.array(list(OrderedDict.fromkeys(map(lambda x: int(x), np.linspace(init_frame, Nentries - 1, int_max))))) # frames at which to calculate the histogram
+
+Ncases = int(np.sqrt(N)) + (1 - int(np.sqrt(N))%2) if Ncases < 0 else Ncases
+points = np.array([(x, y) for y in np.linspace(- box_size/2, box_size/2, Ncases, endpoint=False) for x in np.linspace(- box_size/2, box_size/2, Ncases, endpoint=False)])
+
+
+with gsd.pygsd.GSDFile(open(wrap_file_name, 'rb')) as wrap_file:
+	w_traj = gsd.hoomd.HOOMDTrajectory(wrap_file);
+
+	surface = lambda frame, point: np.sum((np.pi/4)*(w_traj[int(prep_frames + frame)].particles.diameter**2)*(abs((w_traj[int(prep_frames + frame)].particles.position[:, :2] - np.array(point) + box_size/2)%box_size - box_size/2) <= max_box_size/2).all(axis=1)) # get the numbers of particle in the square of centre point and length max_box_size at time frame
+	densities = np.array(list(map(lambda frame: list(map(lambda point: surface(frame, point), points)), frames))).flatten()/(max_box_size**2) # densities
+
+histogram = list(map(lambda bin: (np.sum(densities >= bin/Nbins) - np.sum(densities > (bin + 1)/Nbins))/len(densities), range(Nbins)))
+
+if 'SAVE' in os.environ and eval(os.environ['SAVE']):
+	filename = data_dir + str('/varN_D%s_V%s_R%s_N%s_I%s_M%s_C%s.pickle' % tuple(map(float_to_letters, [density, vzero, dr, N, init_frame, int_max, Ncases]))) # filename
+	with open(filename, 'wb') as dump_file:
+		pickle.dump(histogram, dump_file)
+
+# PLOT
+
+plt.semilogy(np.linspace(0, 1, len(histogram)), histogram, '.')
+plt.title(r'$N=%.2e, \phi=%1.2f, \tilde{v}=%.2e, \tilde{\nu}_r=%.2e$' % (N, density, vzero, dr) + '\n' + r'$S_{init}=%.2e, S_{max}=%.2e, N_{cases}=%.2e$' % (init_frame, int_max, Ncases))
+plt.xlabel(r'$\phi_{loc}$')
+plt.ylabel(r'$P(\phi_{loc})$')
+if 'SAVEFIG' in os.environ and eval(os.environ['SAVEFIG']):
+	figname = data_dir + str('/varN_D%s_V%s_R%s_N%s_I%s_M%s_C%s.eps' % tuple(map(float_to_letters, [density, vzero, dr, N, init_frame, int_max, Ncases]))) # figure anme
+	plt.savefig(figname)
+	print('Figure saved to %s.' % figname)
+if 'SHOW' in os.environ and eval(os.environ['SHOW']):
+	plt.show()
+
--- launch/varN_DPD_P_A.sh
+++ launch/varN_DPD_P_A.sh
@@ -3,17 +3,36 @@ if [[ "$DATA" == "" ]]; then
 	exit 0
 fi
 
-export DATA_DIRECTORY=/home/yketa/hoomd/colmig_DPD_P_A/data/${DATA}
+DATA_DIRECTORY="/home/yketa/hoomd/colmig_DPD_P_A/data/${DATA}"
+export DATA_DIRECTORY=$DATA_DIRECTORY
+
+export INITIAL_FRAME=${INITIAL_FRAME-`/home/yketa/miniconda3/bin/python3.6 <<EOF
+print(int(($(/home/yketa/bin/_colmig_DPD_P_A_data $DATA N_steps)//$(/home/yketa/bin/_colmig_DPD_P_A_data $DATA period_dump))/2))
+EOF
+`}
+export INTERVAL_MAXIMUM=${INTERVAL_MAXIMUM-1}
+export N_CASES=${N_CASES-`/home/yketa/miniconda3/bin/python3.6 <<EOF
+from numpy import sqrt
+N = $(/home/yketa/bin/_colmig_DPD_P_A_data $DATA N)
+print(int(np.sqrt(N)) + (1 - int(np.sqrt(N))%2))
+EOF
+`}
+
+export SHOW='False'
+export SAVE='True'
+
+. /home/yketa/exponents.sh # exporting letters expressions and float conversion functions
+VARN_TIME=varN_I$(float_to_letters $INITIAL_FRAME)_M$(float_to_letters $INTERVAL_MAXIMUM)_C$(float_to_letters $N_CASES)
 
 mkdir -p ${DATA_DIRECTORY}/out
-output_file=${DATA_DIRECTORY}/out/varN.out
+output_file=${DATA_DIRECTORY}/out/${VARN_TIME}.out
 > $output_file
 
-sbatch --job-name=varN_${DATA} ${CHAIN:+-d afterok:$CHAIN} <<EOF
+sbatch --job-name=${VARN_TIME}_${DATA} ${CHAIN:+-d afterok:$CHAIN} <<EOF
 #!/bin/bash
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:k80:1
-#SBATCH --output /home/yketa/hoomd/colmig_DPD_P_A/sub/out/varN_${DATA}.%j.out
+#SBATCH --output /home/yketa/hoomd/colmig_DPD_P_A/sub/out/${VARN_TIME}_${DATA}.%j.out
 #SBATCH --ntasks-per-node 1
 
 /home/yketa/miniconda3/bin/python3.6 /home/yketa/hoomd/scripts/varN.py >> $output_file
