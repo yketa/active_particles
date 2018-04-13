9d177e4ac61a705ff525c9ff52409d7baaa307c5
Yann-Edwin Keta <yketa@users.noreply.github.com>
2018-04-12 19:28:49 -0700

    read different variables

--- plot/CuuLCuuT.py
+++ plot/CuuLCuuT.py
@@ -16,6 +16,10 @@ import matplotlib.colors as colors
 import matplotlib.cm as cmx 
 from collections import OrderedDict
 
+var = os.environ['CORRELATION'] if 'CORRELATION' in os.environ else 'Cww'
+C = {'Cuu':'C_{uu}', 'Cww':'C_{\delta u \delta u}', 'Cdd':'C_{|u||u|}', 'Cee':'C_{\hat{u}\hat{u}}'}[var]
+var += 'b' if not('ENDPOINT' in os.environ and not(eval(os.environ['ENDPOINT']))) else ''
+
 dr_min = float(eval(os.environ['DR_MIN'])) if 'DR_MIN' in os.environ else 1e-10
 dr_max = float(eval(os.environ['DR_MAX'])) if 'DR_MAX' in os.environ else 1e10
 dr_c = float(eval(os.environ['DR_C'])) if 'DR_C' in os.environ else 3e-4
@@ -27,19 +31,27 @@ dt_list = np.array([1, 2, 4, 5, 10, 20, 40, 50, 100, 200, 400, 500, 1000, 2000, 
 # dr_list = np.array([2e-5, 7e-5, 2e-4, 7e-4, 2e-3, 7e-3])
 dirs = [dir for dir in os.listdir() if 'Dk8000_Vj1000' in dir and 'Nq1000_Ll' in dir]
 
-pname = lambda dir: str('%s/param.pickle' % dir)
-fname = lambda dir, t: str('%s/CuuLCuuT_%s_Io5000_T%s_Mn1000_Cn5000.pickle' % (dir, dir[:-7], float_to_letters(t)))
-iname = lambda dir: str('%s/intCuu_%s.pickle' % (dir, dir[:-7]))
+r_min = float(eval(os.environ['R_MIN'])) if 'R_MIN' in os.environ else 1
+r_max = float(eval(os.environ['R_MAX'])) if 'R_MAX' in os.environ else 20
 
-drdtmax, dr, time_step, period_dump, ra, CuuL, CuuT, CwwL, CwwT = {}, {}, {}, {}, {}, {}, {}, {}, {}
+init_frame = float(eval(os.environ['INITIAL_FRAME'])) if 'INITIAL_FRAME' in os.environ else 5000
+N_cases = int(eval(os.environ['N_CASES'])) if 'N_CASES' in os.environ else 500
+int_max = int(eval(os.environ['INTERVAL_MAXIMUM']))if 'INTERVAL_MAXIMUM' in os.environ else 100
+
+pname = lambda dir: str('%s/param.pickle' % dir)
+fname = lambda dir, t: str('%s/%s_%s_I%s_T%s_M%s_C%s.pickle' % ((dir, var, dir[:-7]) + tuple(list(map(float_to_letters, [init_frame, t, int_max, N_cases])))))
+iname = lambda dir: str('%s/int%s_%s_I%s_M%s_C%s_RMIN%s_RMAX%s.pickle' % ((dir, var, dir[:-7]) + tuple(list(map(float_to_letters, [init_frame, int_max, N_cases, r_min, r_max])))))
+
+box_size, drdtmax, dr, time_step, period_dump, ra, CL, CT = {}, {}, {}, {}, {}, {}, {}, {}
 for dir in dirs:
 	with open(iname(dir), 'rb') as i_file:
 		drdtmax[dir] = pickle.load(i_file)[1]
 	with open(pname(dir), 'rb') as p_file:
-		dr[dir], time_step[dir], period_dump[dir] = operator.itemgetter(10, 13, 15)(pickle.load(p_file))
+		box_size[dir], dr[dir], time_step[dir], period_dump[dir] = operator.itemgetter(5, 10, 13, 15)(pickle.load(p_file))
+	ra[dir] = box_size[dir]/N_cases
 	for t in dt_list:
 		with open(fname(dir, t), 'rb') as C_file:
-			ra[(dir, t)], CuuT[(dir, t)], CuuL[(dir, t)], CwwT[(dir, t)], CwwL[(dir, t)] = pickle.load(C_file)
+			CL[(dir, t)], CT[(dir, t)] = pickle.load(C_file)[-2:]
 
 dr_list = sorted(list(OrderedDict.fromkeys([dr[dir] for dir in dr if dr[dir] >= dr_min and dr[dir] <= dr_max])))
 dr0_list = sorted(list(OrderedDict.fromkeys([dr[dir] for dir in dr if dr[dir] < dr_min or dr[dir] > dr_max])))
@@ -68,7 +80,7 @@ def plot(C, CL, CT):
 
 		ax1 = plt.subplot(gs[1, 0])
 		ax1.set_xlabel(r'$\tau_r \equiv \tilde{\nu}_r^{-1}$')
-		ax1.set_ylabel(r'$%s^T/%s^L(\frac{r}{a}=%.3e, \Delta t^*)$' % (C, C, ra[(dirs[0], dt_list[0])]))
+		ax1.set_ylabel(r'$%s^T/%s^L(\frac{r}{a}=%.3e, \Delta t^*)$' % (C, C, ra[dirs[0]]))
 		ax1.plot(np.linspace(1/np.max(np.append(dr_list, dr0_list)), 1/np.min(np.append(dr_list, dr0_list)), 100), [1]*100, color='black', linestyle='--')
 		ax1.set_xscale('log')
 
@@ -86,7 +98,7 @@ def plot(C, CL, CT):
 		leg = plt.subplot(gs[:, 1])
 
 	ax.set_xlabel(r'$\tilde{\nu}_r\Delta t$')
-	ax.set_ylabel(r'$%s^T/%s^L(\frac{r}{a}=%.3e, \Delta t)$' % (C, C, ra[(dirs[0], dt_list[0])]))
+	ax.set_ylabel(r'$%s^T/%s^L(\frac{r}{a}=%.3e, \Delta t)$' % (C, C, ra[dirs[0]]))
 
 	leg.axis('off')
 	lines0 = list(map(lambda d: Line2D([0], [0], color=colors[d], lw=2, label=r'$\tilde{\nu}_r = %.0e$' % d), sorted(np.append(dr_list, dr0_list))))
@@ -121,12 +133,12 @@ def plot(C, CL, CT):
 
 		axL1 = plt.subplot(gs0[0, 1])
 		axL1.set_xlabel(r'$\tau_r \equiv \tilde{\nu}_r^{-1}$')
-		axL1.set_ylabel(r'$%s^L(\frac{r}{a}=%.3e, \Delta t^*)$' % (C, ra[(dirs[0], dt_list[0])]))
+		axL1.set_ylabel(r'$%s^L(\frac{r}{a}=%.3e, \Delta t^*)$' % (C, ra[dirs[0]]))
 		# axL1.plot(np.linspace(1/np.max(np.append(dr_list, dr0_list)), 1/np.min(np.append(dr_list, dr0_list)), 100), [1]*100, color='black', linestyle='--')
 		axL1.set_xscale('log')
 		axT1 = plt.subplot(gs0[1, 1])
 		axT1.set_xlabel(r'$\tau_r \equiv \tilde{\nu}_r^{-1}$')
-		axT1.set_ylabel(r'$%s^T(\frac{r}{a}=%.3e, \Delta t^*)$' % (C, ra[(dirs[0], dt_list[0])]))
+		axT1.set_ylabel(r'$%s^T(\frac{r}{a}=%.3e, \Delta t^*)$' % (C, ra[dirs[0]]))
 		# axT1.plot(np.linspace(1/np.max(np.append(dr_list, dr0_list)), 1/np.min(np.append(dr_list, dr0_list)), 100), [1]*100, color='black', linestyle='--')
 		axT1.set_xscale('log')
 
@@ -140,15 +152,15 @@ def plot(C, CL, CT):
 		leg = plt.subplot(gs0[:, 2])
 
 	else:
-		gs0 = GridSpec(1, 3, width_ratios=[5, 5, 1])
+		gs0 = GridSpec(1, 2, width_ratios=[10, 1])
 		axL = plt.subplot(gs0[0])
 		axT = plt.subplot(gs0[1])
 		leg = plt.subplot(gs0[:, 1])	
 
 	axL.set_xlabel(r'$\tilde{\nu}_r\Delta t$')
-	axL.set_ylabel(r'$%s^L(\frac{r}{a}=%.3e, \Delta t)$' % (C, ra[(dirs[0], dt_list[0])]))
+	axL.set_ylabel(r'$%s^L(\frac{r}{a}=%.3e, \Delta t)$' % (C, ra[dirs[0]]))
 	axT.set_xlabel(r'$\tilde{\nu}_r\Delta t$')
-	axT.set_ylabel(r'$%s^T(\frac{r}{a}=%.3e, \Delta t)$' % (C, ra[(dirs[0], dt_list[0])]))
+	axT.set_ylabel(r'$%s^T(\frac{r}{a}=%.3e, \Delta t)$' % (C, ra[dirs[0]]))
 
 	leg.axis('off')
 	lines0 = list(map(lambda d: Line2D([0], [0], color=colors[d], lw=2, label=r'$\tilde{\nu}_r = %.0e$' % d), sorted(np.append(dr_list, dr0_list))))
@@ -179,8 +191,7 @@ def plot(C, CL, CT):
 		axL1.legend(handles=lines1 + linesL1)
 		axT1.legend(handles=lines1 + linesT1)
 
-plot('C_{uu}', CuuL, CuuT)
-plot('C_{\delta u \delta u}', CwwL, CwwT)
+plot(C, CL, CT)
 
 plt.show()
 
