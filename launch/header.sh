76db208c0ec97161bc581faa6900585d0c8ec452
Yann-Edwin Keta <yketa@users.noreply.github.com>
2018-05-07 15:33:18 -0700

    GPU to GRES

--- launch/header.sh
+++ launch/header.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 PARTITION=${PARTITION-gpu}
-GPU=${GPU-gpu:k80:1}
+GRES=${GRES-gpu:k80:1}
 OUTPUT=${OUTPUT-${PARTITION}.out}
 NTASKS=${NTASKS-1}
 
 cat <<EOF
 #SBATCH --partition=$PARTITION
-#SBATCH --gres=$GPU
+#SBATCH --gres=$GRES
 #SBATCH --output $OUTPUT
 #SBATCH --ntasks-per-node $NTASKS
 EOF
