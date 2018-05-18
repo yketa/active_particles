351fae416ad5592c042b8efc0272f3fd868ca95a
Yann-Edwin Keta <yketa@users.noreply.github.com>
2018-05-18 11:45:20 -0700

    deviation from mean + 2D grid to 1D

--- maths.py
+++ maths.py
@@ -26,3 +26,69 @@ def relative_positions(positions, point, box_size):
 
     return (np.array(positions) - np.array(point)
         + np.array(box_size)/2)%np.array(box_size) - np.array(box_size)/2
+
+def wo_mean(arr):
+    """
+    Returns deviation of values in array with respect to mean of array.
+
+    Parameters
+    ----------
+    arr : array like
+        Array of values.
+
+    Returns
+    -------
+    dev_arr : array like
+        Deviations from mean of array.
+    """
+
+    return np.array(arr) - np.mean(arr, axis=0)
+
+class DictList(dict):
+    """
+    Custom hash table class to give value [] to uninitialised keys.
+    """
+    def __init__(self):
+        super().__init__()
+    def __getitem__(self, key):
+        try:
+            return super().__getitem__(key)
+        except KeyError:
+            return []
+
+def g2Dto1D(C2D, L):
+    """
+    Returns cylindrical average of 2D grid.
+
+    Parameters
+    ----------
+    g2D : 2D array
+        2D grid.
+        NOTE: g2D[0, 0] is considered the r=0 point on the grid, and we
+        consider period boundaries.
+    L : float or float array
+        Length of the box represented by the grid in one dimension or all
+        dimensions.
+
+    Returns
+    -------
+    g1D : Numpy array
+        Array of (r, g1D(r)) with g1D(r) the averaged 2D grid at radius r.
+    """
+
+    g2D = np.array(g2D)
+    dL = np.array(L)/np.array(g2D.shape)    # boxes separation in each direction
+    r_max = np.min(L)/2                     # maximum radius to be calculated in number of boxes
+
+    g1D_dic = DictList()    # hash table of radii and values at radii
+
+    for i in range(g2D.shape[0]):
+        for j in range(g2D.shape[1]):
+            radius = np.sqrt(np.sum((np.array((i, j))*dL)**2))  # radius corresponding to coordinates [i, j], [-i, j], [i, -j], [-i, -j]
+            if radius <= r_max:
+                g1D_dic[radius] += [g2D[i, j], g2D[-i, j], g2D[i, -j],
+                    g2D[-i, -j]]
+
+    return np.array(list(map(
+        lambda radius: [radius, np.mean(g1D_dic[radius])],
+        sorted(g1D_dic))))
