00c19be2974da97a751db0067b9f44f23471847a
Yann-Edwin Keta <yketa@users.noreply.github.com>
2018-06-22 20:51:25 -0700

    tab

--- analysis/css.py
+++ analysis/css.py
@@ -494,7 +494,7 @@ if __name__ == '__main__':	# executing as script
 			Sgrid, Cgrid = tuple(np.transpose(list(map(lambda time:
 				strain_vorticity_grid(parameters['box_size'], Ncases,
 				grid_points, time, dt, w_traj, u_traj, sigma, r_cut)
-				, times)), (1, 0, 2, 3)))					# lists of shear strain and displacement vorticity correlations
+				, times)), (1, 0, 2, 3)))						# lists of shear strain and displacement vorticity correlations
 
 		Css2D, Ccc2D = tuple(map(corField2D_scalar_average, [Sgrid, Cgrid]))	# shear strain and displacement vorticity fields correlations
 
