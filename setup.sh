71d6b1d3796142dacb92ddecda9798865d40db03
Yann-Edwin Keta <yketa@users.noreply.github.com>
2018-06-22 21:07:18 -0700

    aliases are not defined in variables

--- setup.sh
+++ setup.sh
@@ -5,27 +5,28 @@
 # when using active_particles.
 
 export AP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # path to active_particles package
-alias ap_python=$(. activate active_particles_env; which python)  # python executable to use with active_particles
+export AP_PYTHON=$(. activate active_particles_env; which python) # python executable to use with active_particles
+alias ap_python="$AP_PYTHON"
 alias ap_update="( cd $AP_DIR ; git pull )"                       # alias to update active_particles git repository
 
 # PYTHON
 export PYTHONPATH=$PYTHONPATH:${AP_DIR}/..  # for python to find active_particles
 
 # COMMANDS
-alias ap_param="ap_python ${AP_DIR}/param.py"
+alias ap_param="$AP_PYTHON ${AP_DIR}/param.py"
 alias ap_launch="bash ${AP_DIR}/launch/launch.sh"
 
 # SCRIPTS (defined as variables so they can be used with ap_launch)
-export AP_CSS="ap_python ${AP_DIR}/analysis/css.py"
-export AP_CTT="ap_python ${AP_DIR}/analysis/ctt.py"
-export AP_CUU="ap_python ${AP_DIR}/analysis/cuu.py"
-export AP_FRAME="ap_python ${AP_DIR}/analysis/frame.py"
-export AP_MSD="ap_python ${AP_DIR}/analysis/msd.py"
-export AP_VARN="ap_python ${AP_DIR}/analysis/varn.py"
+export AP_CSS="$AP_PYTHON ${AP_DIR}/analysis/css.py"
+export AP_CTT="$AP_PYTHON ${AP_DIR}/analysis/ctt.py"
+export AP_CUU="$AP_PYTHON ${AP_DIR}/analysis/cuu.py"
+export AP_FRAME="$AP_PYTHON ${AP_DIR}/analysis/frame.py"
+export AP_MSD="$AP_PYTHON ${AP_DIR}/analysis/msd.py"
+export AP_VARN="$AP_PYTHON ${AP_DIR}/analysis/varn.py"
 
 # PLOTS
-alias ap_c44="ap_python ${AP_DIR}/plot/c44.py"
-alias ap_pphiloc="ap_python ${AP_DIR}/plot/pphiloc.py"
+alias ap_c44="$AP_PYTHON ${AP_DIR}/plot/c44.py"
+alias ap_pphiloc="$AP_PYTHON ${AP_DIR}/plot/pphiloc.py"
 
 # FUNCTIONS
 . ${AP_DIR}/exponents.sh # translations between floats and litteral expressions
