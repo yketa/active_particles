49d37c59781592a87da750bdeea5b9d073f49933
Yann-Edwin Keta <yketa@users.noreply.github.com>
2018-06-29 16:47:47 -0700

    set_font_size + list_markers

--- plot/plot.py
+++ plot/plot.py
@@ -2,14 +2,34 @@
 Module plot provides useful functions for plots.
 """
 
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.cm as cmx
 import matplotlib.colors as colors
 
+# DEFAULT VARIABLES
+
+_markers = mpl.markers.MarkerStyle.filled_markers   # default markers list
+_linestyles = ('-', '--', '-.', ':')                # default linestyles list
+
+# FUNCTIONS AND CLASSES
+
+def set_font_size(font_size):
+    """
+    Set matplotlib font size.
+
+    Parameters
+    ----------
+    font_size : int
+        Font size.
+    """
+
+    mpl.rcParams.update({'font.size': font_size})
+
 def list_colormap(value_list, colormap='jet'):
     """
     Creates hash table of colors from colormap, defined according to value_list
-    index, with vaulue_list elements as keys.
+    index, with value_list elements as keys.
 
     Parameters
     ----------
@@ -29,4 +49,25 @@ def list_colormap(value_list, colormap='jet'):
     scalarMap = cmx.ScalarMappable(norm=norm, cmap=cmap)        # associates scalar to color
 
     return {value_list[index]: scalarMap.to_rgba(index)
+        for index in range(len(value_list))}
+
+def list_markers(value_list, marker_list=_markers):
+    """
+    Creates hash table of markers from markers_list, defined according to
+    value_list index, with value_list elements as keys.
+
+    Parameters
+    ----------
+    value_list : list
+        List of values.
+    marker_list : list of matplotlib markers
+        List of markers to use. (default: active_particles.plot.plot._markers)
+
+    Returns
+    -------
+    markers : hash table
+        Hash table of markers.
+    """
+
+    return {value_list[index]: marker_list[index]
         for index in range(len(value_list))}
