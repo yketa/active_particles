b4e5b80332b0e0c98cae41a40c1785673386f1cf
Yann-Edwin Keta <yketa@users.noreply.github.com>
2018-07-12 13:59:32 -0700

    different strain correlation computation modes

--- plot/c44.py
+++ plot/c44.py
@@ -3,10 +3,25 @@ Module c44 plots strain correlations projected on cos(4 \\theta) for different
 lag times, which we call C44.
 
 Input files in simulation directory must follow the active_particles.naming.Css
-naming standard.
+(strain correlations computation in real and Fourier space) or the
+active_particles.naming.Ctt and active_particles.naming.Cll (respectively for
+transverse and longitudinal mean square displacements for strain correlations
+computation from these quantities) standards.
 
 Environment modes
 -----------------
+MODE : string
+    Strain correlations computation mode.
+     _________________________________________________________________
+    | Mode    | Computation mode                                      |
+    |_________|_______________________________________________________|
+    | real    | computation in real space                             |
+    |_________|_______________________________________________________|
+    | fourier | computation in Fourier space                          |
+    |_________|_______________________________________________________|
+    | cmsd    | computation from collective mean square displacements |
+    |_________|_______________________________________________________|
+    DEFAULT: real
 TEMPERATURE : bool
     Simulation directory corresponds to a thermal Brownian simulation.
      ________________________________________________
@@ -20,6 +35,10 @@ TEMPERATURE : bool
     DEFAULT: False
 FITTING_LINE : bool
     Display adjustable fitting line on plot.
+    DEFAULT: False
+DIVIDE_BY_MAX : bool
+    Divide C44 by its maximum for each lag-time and display these maxima in
+    graph inset.
     DEFAULT: False
 
 Environment parameters
@@ -38,13 +57,16 @@ INITIAL_FRAME : int
 INTERVAL_MAXIMUM : int
 	Maximum number of intervals of length dt considered for the calculation.
 	DEFAULT: 1
-R_CUT : float
+R_CUT ['real' mode] : float
     Cut-off radius for coarse graining function in terms of particles' mean
     radius.
     DEFAULT: active_particles.analysis.css._r_cut
-SIGMA : float
+SIGMA ['real' mode ]: float
     Length scale of the spatial extent of the coarse graining function.
     DEFAULT: cut-off radius
+R_CUT_FOURIER ['fourier' or 'cmsd' mode] : float
+    Wave length cut-off radius.
+    DEFAULT: active_particles.plot.c44._r_cut_fourier
 N_CASES : int
     Number of boxes in each direction for which shear strain and displacement
     vorticity grids have been computed.
@@ -95,6 +117,12 @@ WSPACE : float
 HSPACE : float
     Plots height space.
     DEFAULT: active_particles.plot.c44._hspace
+WIDTH_INSET [DIVIDE_BY_MAX mode] : float
+    Maximum C44 inset width in percentage of graph width.
+    DEFAULT: active_particles.plot.c44._width_inset
+HEIGHT_INSET [DIVIDE_BY_MAX mode] : float
+    Maximum C44 inset height in percentage of graph height.
+    DEFAULT: active_particles.plot.c44._height_inset
 COLORMAP : string
     Plot colormap.
     DEFAULT: active_particles.plot.c44._colormap
@@ -118,13 +146,17 @@ from os import environ as envvar
 if __name__ == '__main__': envvar['SHOW'] = 'True'
 from os.path import join as joinpath
 
+from active_particles.maths import wave_vectors_2D
 from active_particles.quantities import nD0_active, nD0_thermal
 
-from active_particles.analysis.css import Css2DtoC44, _slope0_c44 as _slope0,\
-    _slope_min_c44 as _slope_min, _slope_max_c44 as _slope_max,\
+from active_particles.analysis.css import Css2DtoC44, StrainCorrelations,\
+    _slope0_c44 as _slope0, _slope_min_c44 as _slope_min,\
+    _slope_max_c44 as _slope_max,\
     _points_x_c44 as _points_x, _points_theta_c44 as _points_theta,\
     _y_min_c44 as _y_min, _y_max_c44 as _y_max,\
-    _r_min_c44 as _r_min, _r_max_c44 as _r_max
+    _r_min_c44 as _r_min, _r_max_c44 as _r_max,\
+    _r_cut
+from active_particles.analysis.ctt import StrainCorrelationsCMSD
 from active_particles.plot.plot import list_colormap
 from active_particles.plot.mpl_tools import FittingLine
 
@@ -140,12 +172,14 @@ from matplotlib.gridspec import GridSpec
 import matplotlib.patches as mpatches
 from matplotlib.lines import Line2D
 
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+
 # DEFAULT VARIABLES
 
-_r_cut = 2	# default cut-off radius for coarse graining function
+_r_cut_fourier = 0  # default cut-off radius for coarse graining function
 
 _font_size = 10     # default plot font size
-_marker_size = 20   # default plot marker size
+_marker_size = 10   # default plot marker size
 
 _ratio_legend = 7   # default width ratio between legend and figure
 _ncol_legend = 1    # default number of columns for the legend
@@ -155,11 +189,23 @@ _hspace = 0.05  # default plots height space
 
 _colormap = 'jet'   # default plot colormap
 
+_width_inset = 30   # default maximum C44 inset width in percentage of graph width
+_height_inset = 30  # default maximum C44 inset height in percentage of graph height
+
 # SCRIPT
 
 if __name__ == '__main__':  # executing as script
 
     # VARIABLES DEFINITIONS
+
+    mode = get_env('MODE', default='real')  # strain correlations computation mode
+
+    if mode == 'real': naming_Css = naming.Css(from_ft=False)       # Css naming object
+    elif mode == 'fourier': naming_Css = naming.Css(from_ft=True)   # Css naming object
+    elif mode == 'cmsd':
+        naming_Ctt = naming.Ctt()                                   # Ctt naming object
+        naming_Cll = naming.Cll()                                   # Cll naming object
+    else: raise ValueError('Mode %s is not known.' % mode)          # mode is not known
 
     data_dir = get_env('DATA_DIRECTORY', default=getcwd())	# data directory
 
@@ -176,6 +222,9 @@ if __name__ == '__main__':  # executing as script
     r_cut = parameters['a']*get_env('R_CUT', default=_r_cut, vartype=float) # cut-off radius for coarse graining function
     sigma = get_env('SIGMA', default=r_cut, vartype=float)                  # length scale of the spatial extent of the coarse graining function
 
+    r_cut_fourier = get_env('R_CUT_FOURIER',
+        default=_r_cut_fourier, vartype=float)  # wave length cut-off radius when computing strain correlations from Fourier transform components
+
     Ncases = get_env('N_CASES', default=ceil(np.sqrt(parameters['N'])),
 		vartype=int)	# number of boxes in each direction for which shear strain and displacement vorticity grids have been computed
 
@@ -187,13 +236,14 @@ if __name__ == '__main__':  # executing as script
     centre = (get_env('X_ZERO', default=0, vartype=float),
 		get_env('Y_ZERO', default=0, vartype=float))	# centre of the box
 
+    divide_by_max = get_env('DIVIDE_BY_MAX', default=False) # divide C44 by its maximum for each lag-time
+
     # NAMING
 
     attributes = {'density': parameters['density'], 'N': parameters['N'],
         'init_frame': init_frame, 'int_max': int_max, 'Ncases': Ncases,
         'r_cut': r_cut, 'sigma': sigma, 'box_size': box_size,
-        'x_zero': centre[0], 'y_zero': centre[1]}   # attributes displayed in filenames
-    naming_Css = naming.Css()                       # Css naming object
+        'x_zero': centre[0], 'y_zero': centre[1]}   # attributes displayed in file names
 
     # PLOT PARAMETERS
 
@@ -217,6 +267,11 @@ if __name__ == '__main__':  # executing as script
 
     colormap = get_env('COLORMAP', default=_colormap)   # plot colormap
 
+    width_inset = get_env('WIDTH_INSET',
+        default=_width_inset, vartype=float)    # maximum C44 inset width in percentage of graph width
+    height_inset = get_env('HEIGHT_INSET',
+        default=_height_inset, vartype=float)   # maximum C44 inset height in percentage of graph height
+
     # CALCULATION PARAMETERS
 
     points_x = get_env('POINTS_X', default=_points_x, vartype=int)              # number of radii at which to evaluate integrated strain correlation
@@ -224,17 +279,65 @@ if __name__ == '__main__':  # executing as script
 
     # CALCULATION
 
-    files = sorted(naming_Css.get_files(directory=data_dir, **attributes))  # files corresponding to parameters
-    dt_list = np.array(list(map(
-        lambda file: naming_Css.get_data(file, 'dt'), files))).flatten()    # list of lag times corresponding to files
-
     toC44 = Css2DtoC44(box_size, points_x, points_theta,
         av_p_sep*r_min, av_p_sep*r_max) # Css2D to C44 conversion object
     C44 = {}                            # hash table of tables of C44 at X with lag times as keys
-    for file, dt in zip(files, dt_list):
-        with open(joinpath(data_dir, file), 'rb') as Css_dump_file:
-            _, Css2D = pickle.load(Css_dump_file)
-        C44[dt] = toC44.get_C44(Css2D)
+
+    if mode == 'real':
+
+        files = naming_Css.get_files(directory=data_dir, **attributes)          # files corresponding to parameters
+        dt_list = np.array(list(map(
+            lambda file: naming_Css.get_data(file, 'dt'), files))).flatten()    # list of lag times corresponding to files
+
+        for file, dt in zip(files, dt_list):
+            with open(joinpath(data_dir, file), 'rb') as Css_dump_file:
+                _, Css2D = pickle.load(Css_dump_file)
+            C44[dt] = toC44.get_C44(Css2D)
+
+    elif mode == 'fourier':
+
+        files = naming_Css.get_files(directory=data_dir, **attributes)          # files corresponding to parameters
+        dt_list = np.array(list(map(
+            lambda file: naming_Css.get_data(file, 'dt'), files))).flatten()    # list of lag times corresponding to files
+
+        wave_vectors = wave_vectors_2D(Ncases, Ncases, d=box_size/Ncases)   # wave vectors at which Fourier transform was calculated
+
+        for file, dt in zip(files, dt_list):
+            with open(joinpath(data_dir, file), 'rb') as Css_dump_file:
+                FFTsgridsqnorm = pickle.load(Css_dump_file)
+            C44[dt] = toC44.get_C44(
+                StrainCorrelations(wave_vectors, FFTsgridsqnorm)
+                .strain_correlations(r_cut=r_cut_fourier))
+
+    elif mode == 'cmsd':
+
+        files_Ctt = naming_Ctt.get_files(directory=data_dir, **attributes)      # files corresponding to parameters
+        dt_list_Ctt = np.array(list(map(
+            lambda file: naming_Ctt.get_data(file, 'dt'), files))).flatten()    # list of lag times corresponding to files
+
+        files_Cll = naming_Cll.get_files(directory=data_dir, **attributes)
+        dt_list_Cll = np.array(list(map(
+            lambda file: naming_Cll.get_data(file, 'dt'), files))).flatten()    # list of lag times corresponding to files
+
+        files, dt_list = [], []
+        for file_Ctt, dt in zip(files_Ctt, dt_list_Ctt):
+            if dt in dt_list_Cll:   # looking for transversal and longitudinal collective mean square displacements files with same lag times
+                files += [(file_Ctt, files_Cll[dt_list_Cll.index(dt)])]
+                dt_list += [dt]
+
+        wave_vectors = wave_vectors_2D(Ncases, Ncases, d=box_size/Ncases)   # wave vectors at which Fourier transform was calculated
+
+        for (file_Ctt, file_Cll), dt in zip(files, dt_list):
+            with open(joinpath(data_dir, file_Ctt), 'rb') as Ctt_dump_file,\
+                open(joinpath(data_dir, file_Cll), 'rb') as Cll_dump_file:
+                _, k_cross_FFTugrid2D_sqnorm, _ = pickle.load(Ctt_dump_file)
+                _, k_dot_FFTugrid2D_sqnorm, _ = pickle.load(Cll_dump_file)
+            C44[dt] = toC44.get_C44(
+                StrainCorrelationsCMSD(wave_vectors,
+                    k_cross_FFTugrid2D_sqnorm, k_dot_FFTugrid2D_sqnorm)
+                .strain_correlations(r_cut=r_cut_fourier))
+
+    dt_list = sorted(dt_list)
 
     # PLOT
 
@@ -251,9 +354,16 @@ if __name__ == '__main__':  # executing as script
     leg.axis('off')
 
     ax.set_xlabel(r'$r/a$' + ' ' + r'$(a = L/\sqrt{N})$')
-    ax.set_ylabel(r'$C_4^4(r) = \frac{1}{\pi}\int_0^{2\pi}d\theta$'
-        + ' ' + r'$C_{\epsilon_{xy}\epsilon_{xy}}(r, \theta)$'
-        + ' ' + r'$\cos4\theta$')
+    if divide_by_max:
+        ax.set_ylabel(
+            r'$C_4^4(r)/max(C_4^4(r), %.2e \leq r \leq %.2e)$' % (r_min, r_max)
+            + '\n' + r'$C_4^4(r) = \frac{1}{\pi}\int_0^{2\pi}d\theta$'
+            + ' ' + r'$C_{\epsilon_{xy}\epsilon_{xy}}(r, \theta)$'
+            + ' ' + r'$\cos4\theta$')
+    else:
+        ax.set_ylabel(r'$C_4^4(r) = \frac{1}{\pi}\int_0^{2\pi}d\theta$'
+            + ' ' + r'$C_{\epsilon_{xy}\epsilon_{xy}}(r, \theta)$'
+            + ' ' + r'$\cos4\theta$')
     ax.set_xlim([r_min, r_max])
     ax.set_ylim([y_min, y_max])
     ax.set_yscale('log')
@@ -268,31 +378,40 @@ if __name__ == '__main__':  # executing as script
         fitting_line = FittingLine(ax, slope0, slope_min, slope_max,
             x_fit='(r/a)', y_fit='C_4^4(r/a)')
 
-    for dt in dt_list:
-        ax.loglog(C44[dt][:, 0]/av_p_sep, C44[dt][:, 1],
-            color=colors[dt], label=r'$\Delta t = %.0e$'
-            % (parameters['period_dump']*parameters['time_step']*dt))
-
     if get_env('TEMPERATURE', default=False, vartype=bool): # TEMPERATURE mode
         nD0 = nD0_thermal(parameters['N'], parameters['kT'],
             parameters['damp_bro']*parameters['a'], parameters['box_size'])
         legend0 = [mpatches.Patch(color='none',
             label=r'$nD_0 = \frac{2 k_B T N}{\lambda a L^2} = %.2e$' % nD0)]
-        legend0 += list(map(lambda dt: Line2D([0], [0], color=colors[dt],
-            label=r'$nD_0\Delta t = %.2e$'
-            % (dt*parameters['time_step']*parameters['period_dump']*nD0)),
-            dt_list))
     else:
         nD0 = nD0_active(parameters['N'], parameters['vzero'],
             parameters['dr'], parameters['box_size'])
         legend0 = [mpatches.Patch(color='none', label=
             r'$nD_0 = \frac{Nv_0^2}{2\nu_r L^2} = %.2e$' % nD0)]
-        legend0 += list(map(lambda dt: Line2D([0], [0], color=colors[dt],
-            label=r'$nD_0\Delta t = %.2e$'
-            % (dt*parameters['time_step']*parameters['period_dump']*nD0)),
-            dt_list))
+    legend0 += list(map(lambda dt: Line2D([0], [0], color=colors[dt],
+        label=r'$nD_0\Delta t = %.2e$'
+        % (dt*parameters['time_step']*parameters['period_dump']*nD0)),
+        dt_list))
     legend0 += [Line2D([0], [0], lw=0, label='')]
     leg.legend(handles=legend0, loc='center', ncol=ncol_legend)
+
+    if divide_by_max:   # DIVIDE_BY_MAX mode
+        inset_ax = inset_axes(ax, loc=1,
+            width='%e' % width_inset + '%', height='%e' % height_inset + '%')   # inset axes
+        inset_ax.set_yscale('log')
+        inset_ax.set_ylabel(r'$max(C_4^4(r))$')
+        inset_ax.set_xscale('log')
+        inset_ax.set_xlabel(r'$nD_0\Delta t$')
+
+    for dt in dt_list:
+        if divide_by_max:
+                max_C44 = max(C44[dt][:, 1])
+                C44[dt][:, 1] /= max_C44
+                inset_ax.scatter(
+                    dt*parameters['time_step']*parameters['period_dump']*nD0,
+                    max_C44,
+                    color=colors[dt])
+        ax.plot(C44[dt][:, 0]/av_p_sep, C44[dt][:, 1], color=colors[dt])
 
     title = r'$N=%.2e, \phi=%1.2f,$' % (parameters['N'], parameters['density'])
     if get_env('TEMPERATURE', default=False, vartype=bool): # TEMPERATURE mode
@@ -300,10 +419,15 @@ if __name__ == '__main__':  # executing as script
     else:
         title += r'$\tilde{v}=%.2e, \tilde{\nu}_r=%.2e$' % (
             parameters['vzero'], parameters['dr'])
+    title += r'$, L=%.2e, x_0=%.2e, y_0=%.2e$' % (box_size, *centre)
     title += '\n'
     title += r'$S_{init}=%.2e, S_{max}=%.2e,$' % (init_frame, int_max)
-    title += r'$N_{cases}=%.2e, r_{cut}=%.2e, \sigma=%.2e$' % (Ncases, r_cut,
-        sigma)
+    title += (r'$N_{cases}=%.2e, dL=%.2e a$'
+        % (Ncases, np.sqrt(parameters['N'])/Ncases))
+    if mode == 'real':
+        title += r'$, r_{cut}=%.2e, \sigma=%.2e$' % (r_cut, sigma)
+    else:
+        title += r'$, r_{cut}=%.2e$' % r_cut_fourier
     title += '\n'
     title += r'$N_r=%.2e, N_{\theta}=%.2e$' % (points_x, points_theta)
     if 'BOX_SIZE' in envvar:
